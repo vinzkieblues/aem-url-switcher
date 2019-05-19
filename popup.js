5456ac76e97a864b8f09d8d24b4b132ac367b0b1
vinzkieblues <vinzkie457@gmail.com>
2019-05-19 11:56:24 +0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -23,13 +23,22 @@ switchBtns.forEach((element, index) => {
 })
 
 function onWindowLoad(){
-  let currentUrl = ''
+  let currentUrl = ''  
   removeActive(switchBtns)
   chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
-    currentUrl = checkUrl(tab[0].url)
+    currentUrl = checkUrl(tab[0].url)    
     switchBtns.forEach((element, index) => {
-      if(element.value == currentUrl){
-        element.classList.add("active")
+      if(currentUrl =='siteAdmin'){
+        element.classList.add("disabled")
+        element.disabled = true
+        if(element.value == currentUrl){
+          element.classList.add("hidden")
+        }
+      }
+      else{
+        if(element.value == currentUrl){
+          element.classList.add("active")
+        }
       }
     })
   })
@@ -51,34 +60,50 @@ function switchModeTo(currentMode,toEnv,url){
   else if(currentMode =='author' && toEnv =='publish'){
     return url.replace(/author-|editor.html\/|\?wcmmode=disabled/g,'')
   }
+  else if(currentMode =='author' && toEnv =='siteAdmin'){
+    return url.replace(/editor.html/g,'sites.html').replace(/\.html+$/g,'')
+  }
+
   else if(currentMode =='viewPublish' && toEnv =='viewPublish'){
     return url
   }
   else if(currentMode =='viewPublish' && toEnv =='author'){
-    let x = url.slice(0, url.indexOf('content'))
+    let x = url.slice(0, url.indexOf('content/informa/'))
     return url.replace(x,x+'editor.html/').replace(/\?wcmmode=disabled/,'')
   }
   else if(currentMode =='viewPublish' && toEnv =='publish'){
     return url.replace(/author-|editor.html\/|\?wcmmode=disabled/g,'')
   }
+  else if(currentMode =='viewPublish' && toEnv =='siteAdmin'){
+    let x = url.slice(0, url.indexOf('content/informa/'))
+    return url.replace(x,x+'sites.html/').replace(/\.html\?wcmmode=disabled/,'')
+  }
+
   else if(currentMode =='publish' && toEnv =='publish'){
     return url
   }
   else if(currentMode =='publish' && toEnv =='author'){
     let y = url.slice(0, url.indexOf('informa-'))
     let url2 = url.replace(y,y+'author-')
-    let x = url2.slice(0, url2.indexOf('content'))
+    let x = url2.slice(0, url2.indexOf('content/informa/'))
     return url2.replace(x,x+'editor.html/')
   }
   else if(currentMode =='publish' && toEnv =='viewPublish'){
     let y = url.slice(0, url.indexOf('informa-'))
     return url.replace(y,y+'author-') + '?wcmmode=disabled'
   }
+  else if(currentMode =='publish' && toEnv =='siteAdmin'){
+    let y = url.slice(0, url.indexOf('informa-'))
+    let url2 = url.replace(y,y+'author-')
+    let x = url2.slice(0, url2.indexOf('content/informa/'))
+    return url2.replace(x,x+'sites.html/').replace(/\.html+$/g,'')
+  }
 }
 
 function checkUrl(url) {
   let pub = '?wcmmode=disabled'
   let author = 'editor.html/'
+  let admin = 'sites.html/'
 
   if(url.indexOf(author) >= 0){
     return 'author'
@@ -86,9 +111,13 @@ function checkUrl(url) {
   else if(url.indexOf(pub) >= 0){
     return 'viewPublish'
   }
-  else if(url.indexOf(author) < 0 && url.indexOf(pub) < 0){
+  else if(url.indexOf(admin) >= 0){
+    return 'siteAdmin'
+  }
+  else if(url.indexOf(author) < 0 && url.indexOf(pub) < 0 && url.indexOf(admin) < 0){
     return 'publish'
   }
+  
 }
 
 
